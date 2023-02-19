ff8ff7e09d1c6a9a47478400aaca7a44b521befb
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-02-19 15:22:14 +0530

    Add Title

--- src/App.js
+++ src/App.js
@@ -26,11 +26,15 @@ function App() {
       setMode('dark');
       //change the backgroud color in darkmode
       document.body.style.backgroundColor = '#042743';
-      showAlert("Dark mode has been enabled","success")
+      showAlert("Dark mode has been enabled","success");
+      //change the title
+      //document.title = 'TextUtils - Dark Mode';
     } else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
-      showAlert("Light mode has been enabled","success")
+      showAlert("Light mode has been enabled","success");
+      //change the title
+      //document.title = 'TextUtils - Light Mode';
     }
 
   }
